ff77d1fd23cf4ee04e8c699f1139b8e7535df7c1
Robby Ranshous <rranshous@gmail.com>
2012-03-22 15:05:58 -0400

    updating reuse connect functions

--- lib/service_client.py
+++ lib/service_client.py
@@ -8,11 +8,14 @@ from thrift.protocol import TBinaryProtocol
 
 import random
 
+import threading
+
 DISCOVERY_HOST = '127.0.0.1'
 DISCOVERY_PORT = 9191
 
-client_lookup = {}
-transport_lookup = {}
+global_client_lookup = {}
+global_transport_lookup = {}
+endpoint_lookup = {}
 
 # when we exit clean up our
 def cleanup_transports():
@@ -62,6 +65,18 @@ def connect_discovery():
 @contextmanager
 def connect_reuse(service,host=None,port=None,rediscover=True):
 
+    global endpoint_lookup
+    global global_transport_lookup
+    global global_client_lookup
+
+    thread = threading.current_thread()
+    if thread:
+        thread = thread.get_ident()
+
+    # keep our lookup thread specific
+    client_lookup = global_client_lookup.get(thread,{})
+    transport_lookup = global_transport_lookup.get(thread,{})
+
     if rediscover and client_lookup.get(service):
         # refresh the client
         transport_lookup.get(service).close()
@@ -69,35 +84,40 @@ def connect_reuse(service,host=None,port=None,rediscover=True):
         del client_lookup[service]
 
     if not client_lookup.get(service):
-        # if we didn't get a host / port use service
-        # discovery to find it
-        with connect_discovery() as c:
-            service_name = service.__name__.split('.')[-1]
-            service_details = c.find_service(service_name)
-            assert service_details, "Could not find service in discovery"
-            port = service_details.port
-            host = service_details.host
 
-        transport = TSocket.TSocket(host,port)
-        transport = TTransport.TBufferedTransport(transport)
-        protocol = TBinaryProtocol.TBinaryProtocol(transport)
-        client = getattr(service,'Client')(protocol)
-        client_lookup[service] = client
-        transport_lookup[service] = transport
-        transport.open()
+        if not host or not port and service in endpoint_lookup:
+            endpoint_list = endpoint_lookup.get(service)
+            if endpoint_list:
+                host, port = random.sample(endpoint_list,1)[0]
+
+        if not host or not port:
+
+            # if we didn't get a host / port use service
+            # discovery to find it
+            with connect_discovery() as c:
+                service_name = service.__name__.split('.')[-1]
+                service_details = c.find_service(service_name)
+                assert service_details, "Could not find service in discovery"
+                port = service_details.port
+                host = service_details.host
+
+            transport = TSocket.TSocket(host,port)
+            transport = TTransport.TBufferedTransport(transport)
+            protocol = TBinaryProtocol.TBinaryProtocol(transport)
+            client = getattr(service,'Client')(protocol)
+            client_lookup[service] = client
+            transport_lookup[service] = transport
+            transport.open()
 
     yield client_lookup.get(service)
 
-
-known_good = {}
-
 @contextmanager
-def connect(service,host=None,port=None):
+def connect_no_reuse(service,host=None,port=None):
 
-    global known_good
+    global endpoint_lookup
 
-    if not host or not port and service in known_good:
-        endpoint_list = known_good.get(service)
+    if not host or not port and service in endpoint_lookup:
+        endpoint_list = endpoint_lookup.get(service)
         if endpoint_list:
             host, port = random.sample(endpoint_list,1)[0]
 
@@ -115,6 +135,8 @@ def connect(service,host=None,port=None):
     client = getattr(service,'Client')(protocol)
     transport.open()
     yield client
-    known_good.setdefault(service,[]).append((host,port))
+    endpoint_lookup.setdefault(service,[]).append((host,port))
     transport.close()
 
+
+connect = connect_no_reuse
